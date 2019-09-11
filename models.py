e16c8d50f91fc3652cfda8db8bcf8e0f9eda57ae
AlexLinus <kelevra141993@gmail.com>
2019-09-11 23:50:14 +0600

    added relationship between CV and Education

--- models.py
+++ models.py
@@ -1,6 +1,12 @@
 import datetime
 from app import app, db
 
+
+cv_education = db.Table('cv_education',
+    db.Column('cv_id', db.Integer, db.ForeignKey('CV.id')),
+    db.Column('education_id', db.Integer, db.ForeignKey('education.id'))
+    )
+
 class CV(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
@@ -16,3 +22,11 @@ class CV(db.Model):
     position = db.Column(db.String(100), nullable=True)
     #https://sqlalchemy-utils.readthedocs.io/en/latest/data_types.html#module-sqlalchemy_utils.types.choice, но нужно переопредялять db на sqlaclhemy_utils вместо SQLACLCHEMY в app.py
     work_schedule = db.Column(db.String(100), default='Полный рабочий день')
+    education = db.relationship('Education', secondary=cv_education, backref=db.backref('cv', cascade="all, delete-orphan", lazy='dynamic'))
+
+class Education(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    study_place = db.Column(db.String(200), nullable=False)
+    specialty_faculty = db.Column(db.String(200), nullable=True)
+    start_study = db.Column(db.Date(), nullable=True)
+    end_study = db.Column(db.Date(), nullable=True)
